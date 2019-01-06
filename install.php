11eb8e706385bf3734a66eb568cbc612b9c05b3e
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:02:51 -0500

    New cl-publish system.

--- install.php
+++ install.php
@@ -4,4 +4,4 @@
  * Used by the site installer
  */
 
-return new CL\Site\Install\Package('\CL\Examples\ExamplesPlugin', '/cldist');
+return new CL\Site\Install\Package('\CL\Examples\ExamplesPlugin', '/dist');
